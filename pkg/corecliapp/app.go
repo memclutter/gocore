a726ff457e3260d58d91a0fc0b3f502a65ffddcf
Memory Clutter <memclutter@gmail.com>
2021-12-12 01:56:31 +0300

    Added commands wrapper for cli

--- pkg/corecliapp/app.go
+++ pkg/corecliapp/app.go
@@ -1,7 +1,9 @@
 package corecliapp
 
 import (
+	"github.com/memclutter/gocore/pkg/corestrings"
 	"github.com/urfave/cli/v2"
+	"reflect"
 )
 
 // App godoc
@@ -25,5 +27,24 @@ func NewApp() *App {
 // Parse and set up application config
 func (app *App) CoreConfig(config interface{}) *App {
 	app.Flags = ConfigToFlags(config)
+	return app
+}
+
+type CoreCommand interface {
+	Run(c *cli.Context) error
+	Flags() []cli.Flag
+}
+
+func (app *App) CoreCommands(commands []CoreCommand) *App {
+	app.Commands = cli.Commands{}
+	for _, c := range commands {
+		name := corestrings.ToLowerFirst(reflect.ValueOf(c).Type().Name())
+
+		app.Commands = append(app.Commands, &cli.Command{
+			Name:   name,
+			Flags:  c.Flags(),
+			Action: c.Run,
+		})
+	}
 	return app
 }
